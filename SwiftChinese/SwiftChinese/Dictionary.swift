4ad5cc3a3c4646df31309dfaf4584545a8e44410
Niklas Berglund <niklas@asdasd.net>
2016-12-13 22:46:36 +0800

    Cleaned up Dictionary

--- SwiftChinese/SwiftChinese/Dictionary.swift
+++ SwiftChinese/SwiftChinese/Dictionary.swift
@@ -10,48 +10,5 @@ import UIKit
 import CoreData
 
 public class Dictionary: NSObject {
-    enum UpdateStatus {
-        case success
-        case failure
-    }
-    
-    typealias CompletionClosure = (UpdateStatus) -> Void
-    typealias ProgressClosure = (_ current: Int, _ total : Int) -> Void
-    
     var dataController = DataController()
-    
-    // MARK: - For debugging
-    
-    public func storeTestEntry() -> Void {
-        let context = dataController.getContext()
-        
-        let entry = NSEntityDescription.insertNewObject(forEntityName: "Entry", into: context) as! Entry
-        let chineseEntry = NSEntityDescription.insertNewObject(forEntityName: "ChineseEntry", into: context) as! ChineseEntry
-        let englishEntry = NSEntityDescription.insertNewObject(forEntityName: "EnglishEntry", into: context) as! EnglishEntry
-        
-        chineseEntry.pinyin = "mao1"
-        chineseEntry.simplified = "猫"
-        chineseEntry.traditional = "貓"
-        
-        englishEntry.english = "cat"
-        
-        var englishSet = Set<EnglishEntry>()
-        englishSet.insert(englishEntry)
-
-        entry.inChinese = chineseEntry
-        entry.inEnglish = englishSet as NSSet
-        
-        entry.added = Date() as NSDate
-        entry.lineHash = "dummy-hash".md5()
-        entry.lastMofified = Date() as NSDate
-        
-        do {
-            try context.save()
-            print("Saved")
-        } catch let error as NSError {
-            print(error)
-        } catch {
-            print("?")
-        }
-    }
 }
