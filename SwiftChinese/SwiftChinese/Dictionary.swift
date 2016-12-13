339fa7b96a11c34cc1c92a8e847964c4690eebb7
Niklas Berglund <niklas@asdasd.net>
2016-12-13 20:35:19 +0800

    Migrated Core Data insertion from Dictionary to Importer

--- SwiftChinese/SwiftChinese/Dictionary.swift
+++ SwiftChinese/SwiftChinese/Dictionary.swift
@@ -20,44 +20,6 @@ public class Dictionary: NSObject {
     
     var dataController = DataController()
     
-    public func insert(translation: Translation) -> Void {
-        let context = dataController.getContext()
-        
-        let entry = NSEntityDescription.insertNewObject(forEntityName: "Entry", into: context) as! Entry
-        let chineseEntry = NSEntityDescription.insertNewObject(forEntityName: "ChineseEntry", into: context) as! ChineseEntry
-        
-        chineseEntry.pinyin = translation.pinyin
-        chineseEntry.simplified = translation.simplifiedChinese
-        chineseEntry.traditional = translation.traditionalChinese
-        
-        var englishEntries = Set<EnglishEntry>()
-        
-        for englishDefinition in translation.englishDefinitions {
-            debugPrint(englishDefinition)
-            
-            let englishEntry = NSEntityDescription.insertNewObject(forEntityName: "EnglishEntry", into: context) as! EnglishEntry
-            
-            englishEntry.english = englishDefinition
-            englishEntries.insert(englishEntry)
-        }
-        
-        entry.inChinese = chineseEntry
-        entry.inEnglish = englishEntries as NSSet
-        
-        entry.added = Date() as NSDate
-        entry.lineHash = translation.lineHash
-        entry.lastMofified = Date() as NSDate
-        
-        do {
-            try context.save()
-            print("Saved")
-        } catch let error as NSError {
-            print(error)
-        } catch {
-            print("?")
-        }
-    }
-    
     // MARK: - For debugging
     
     public func storeTestEntry() -> Void {
--- SwiftChinese/SwiftChinese/Importer.swift
+++ SwiftChinese/SwiftChinese/Importer.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 public class Importer: NSObject {
+    var dataController : DataController
     var dictionaryExport : DictionaryExport
     
     public init(dictionaryExport: DictionaryExport) {
+        self.dataController = DataController()
         self.dictionaryExport = dictionaryExport
     }
     
-    public func insertAllEntries(dictionary: Dictionary) -> Void {
+    public func insertOrUpdate(fromDictionaryExport: DictionaryExport) -> Void {
         let translationArray = translationObjects(fromDictionaryString: self.dictionaryExport.content!)
         
-        for translation in translationArray {
-            dictionary.insert(translation: translation)
+        for translationObject in translationArray {
+            self.insert(translation: translationObject)
         }
     }
     
@@ -45,4 +48,42 @@ public class Importer: NSObject {
         return translationObjects
     }
     
+    // MARK: - Core data store methods
+    public func insert(translation: Translation) -> Void {
+        let context = dataController.getContext()
+        
+        let entry = NSEntityDescription.insertNewObject(forEntityName: "Entry", into: context) as! Entry
+        let chineseEntry = NSEntityDescription.insertNewObject(forEntityName: "ChineseEntry", into: context) as! ChineseEntry
+        
+        chineseEntry.pinyin = translation.pinyin
+        chineseEntry.simplified = translation.simplifiedChinese
+        chineseEntry.traditional = translation.traditionalChinese
+        
+        var englishEntries = Set<EnglishEntry>()
+        
+        for englishDefinition in translation.englishDefinitions {
+            debugPrint(englishDefinition)
+            
+            let englishEntry = NSEntityDescription.insertNewObject(forEntityName: "EnglishEntry", into: context) as! EnglishEntry
+            
+            englishEntry.english = englishDefinition
+            englishEntries.insert(englishEntry)
+        }
+        
+        entry.inChinese = chineseEntry
+        entry.inEnglish = englishEntries as NSSet
+        
+        entry.added = Date() as NSDate
+        entry.lineHash = translation.lineHash
+        entry.lastMofified = Date() as NSDate
+        
+        do {
+            try context.save()
+            print("Saved")
+        } catch let error as NSError {
+            print(error)
+        } catch {
+            print("?")
+        }
+    }
 }
